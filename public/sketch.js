bb5bbe01bbe9c647f5015442b5afdc0fa469dfb1
michal2003pluk <michalpl2003@gmail.com>
2020-05-14 15:09:20 +0100

    Comm between client and server functionality + fixes

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,32 @@
+var socket;
 function setup() {
   createCanvas(800, 800);
   background(51);
 
-var socket;
   socket = io.connect('http://localhost:3000');
+socket.on('mouse', newDrawing);
+
+}
+
+function newDrawing(data) {
+  noStroke()
+  fill(0, 100, 255)
+  ellipse(data.x, data.y, 20, 20);
+}
+
+function mouseDragged() {
+console.log('Sending:' + mouseX + ',' + mouseY);
+
+var data = {
+x: mouseX,
+y: mouseY
+}
+socket.emit('mouse', data)
+
+noStroke()
+fill(255, 255, 255)
+ellipse(mouseX, mouseY, 20, 20);
 }
 
 function draw() {
-  noStroke()
-  ellipse(mouseX, mouseY, 50, 50);
 }
--- server.js
+++ server.js
@@ -13,4 +13,15 @@ io.sockets.on('connection', newConnection); //checking if there is a new connect
 
 function newConnection(socket) {
   console.log('new connection: ' + socket.id);
+
+  socket.on('mouse', mouseMsg);
+
+  function mouseMsg(data) {
+    socket.broadcast.emit('mouse', data);   //emits the data to all clients except the sender
+    // io.sockets.emit('mouse', data);      //emits data to all clients including the sender
+    console.log(data);
+  }
+
+
+
 }
