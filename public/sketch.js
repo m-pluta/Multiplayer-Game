0a83baaa3cd2ddd6c3983080ff1297ec31ee4c64
michal2003pluk <michalpl2003@gmail.com>
2020-05-14 02:32:24 +0100

    node server set-up

---
+++ public/sketch.js
@@ -0,0 +1,13 @@
+
+//var socket
+function setup() {
+createCanvas(800, 800);
+  background(51);
+
+  //socket = socket.io.connect('http://localhost:3000')
+}
+
+function draw() {
+  noStroke()
+  ellipse(mouseX, mouseY, 50, 50);
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'); //importing library
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("Socket server initialised");
+
+var socket = require('socket.io'); //importing library
+var io = socket(server);
+
+io.sockets.on('connection', newConnection); //checking if there is a new connection
+
+function newConnection(socket) {
+  console.log('new connection')
+console.log(socket)
+
+
+}
